779ff17241e1f29bc72e5b80124779281b21f23f
andigandhi <andigrasser13@gmail.com>
2021-03-15 18:14:30 +0100

    Drastically changed the firmware

---
+++ TeensyMixxx/HARDWARE.md
@@ -0,0 +1,8 @@
+# Some words about the wiring
+The TeensyMIDI uses three 16pin multiplexers (MUX).
+
+MUX1 deals with the 2 effect channels (each with 4 Knobs and 4 buttons)
+
+MUX2 deals with the LOW/MID/HIGH mixer section and the HIGH-/LOW-pass filter
+
+MUX3 deals with the "button section" of deck 1/3 and the "volume section" (in total 4 sliders and 12 buttons)
--- TeensyMixxx/TeensyMixxx.ino
+++ TeensyMixxx/TeensyMixxx.ino
@@ -1,9 +1,14 @@
 #include <TimerOne.h>
 #include <Encoder.h>
 
-//CHANNEL
-int channelNumber = 1; //each module should have a unique channel number between 1 and 125 (126 & 127 are used by the encoders).
-int pbBounce = 150; //150 millisecond debounce duration - you may want to change this value depending on your pushbuttons
+//Channel number for the TeensyMIDI
+#define channelNumber 1
+
+//Time to debounce the buttons in ms
+#define pbBounce 150
+
+// The status of the two shift buttons (left and right deck)
+bool shiftKey[2] = {false, false};
 
 //PUSHBUTTON
 // '1' == Button verbunden
@@ -20,74 +25,26 @@ int toReadPushButton[38] =
   0, 0, 0, 0, 0, //30-34
   0, 9, 9     //35-37
 };
-//pushbutton mode
-//there are a few different modes in which you may wish for your pushbutton to behave
-//'1' - standard mode - when pushbutton is engaged note is turned on, when pushbutton is released, note is turned off
-//'2' - on mode - note is only turned on with each click
-//'3' - off mode - note is only turned off with each click
-//'4' - toggle mode - note is switched between on and off with each click
-//pins with '9' are those which are encoders and should not be used as pushbuttons unless necessary
-int pushbuttonMode[76] =
-{ //Pin number are written below
-  9, 9, 9, 9, 1, //0-4
-  1, 9, 1, 1, 1, //5-9
-  1, 1, 1, 1, 0, //10-14
-  0, 0, 0, 9, 9, //15-19
-  1, 1, 1, 1, 1, //20-24
-  0, 0, 0, 0, 0, //25-29
-  0, 0, 0, 0, 0, //30-34
-  0, 9, 9,    //35-37
-  9, 9, 9, 9, 0, //38-42 SHIFT
-  0, 9, 0, 0, 0, //43-47 SHIFT
-  0, 0, 0, 0, 0, //48-52 SHIFT
-  0, 0, 0, 9, 9, //53-57 SHIFT
-  0, 0, 0, 0, 0, //58-62 SHIFT
-  0, 0, 0, 0, 0, //63-67 SHIFT
-  0, 0, 0, 0, 0, //68-72 SHIFT
-  0, 9, 9     //73-75 SHIFT
-};
-
-//SHIFT
-int shiftPin = 0; //if using a shift button enter the pin number here, else put 0
-
-//ANALOG IN MUX
-//CD4067BE - http://www.ti.com/lit/ds/symlink/cd4067b.pdf
-//'1' for multiplexer inputs you want to read, else enter '0'
-int toReadMux[16] =
-{ //IC pin number are written below
-  1, 1, 1, 1, //4-7
-  1, 1, 1, 1,
-  2, 2, 2, 2,
-  2, 2, 2, 2
-};
 
 int MUXupdateCounter = 15;
 
 
 
 // VARIABLES AND FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-//PUSHBUTTONS
-long timeHit[76]; //38*2 = 76 (shift button)
-boolean buttonState[76 + 8]; //stored state: if the button was last turned on or off
-int shiftChange;
-//ENCODER
-int encoderPins[4][2] = {{0, 1}, {2, 3}, {36, 37}, {18, 19}}; //encoder pin numbers
+
+//ENCODERS
 Encoder enc1(0, 1);
 Encoder enc2(2, 3);
 Encoder enc3(18, 19);
 
-//ANALOG IN
-int s0 = 14; //control pin A
-int s1 = 15; //control pin B
-int s2 = 16; //control pin C
-int s3 = 17; //control pin D
+// MUX STUFF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 int SIG_pin = 43; //read pin
-int analogInsPrev[48]; //array to hold previously read analog values - set all to zero for now
 int tempAnalogIn = 0; //array to hold previously read analog values
 int tempAnalogInMap = 0;
-int analogThreshold = 4; //threshold
-int controlPin[] = {s0, s1, s2, s3}; //set contol pins in array
-//control array
+int analogThreshold = 20; //threshold
+int controlPin[] = {14, 15, 16, 17}; //set contol pins in array
+
+//control array for the mux pins
 int muxChannel[16][4] = {
   {0, 0, 0, 0}, //channel 0
   {1, 0, 0, 0}, //channel 1
@@ -107,14 +64,21 @@ int muxChannel[16][4] = {
   {1, 1, 1, 1} //channel 15
 };
 
-//LED-Status
+//LED-Status blink ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 bool ledStatus[2];
 bool syncStatus[2];
 bool blinkMode[2];
 bool loopOn[2];
 
+
+
+// CODE BEGIN  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+
+// Read an analog value from a Teensy analog pin
 int readPoti(int num) {
   int out = 0;
+  // Do 3 measurements and return the mean value
   for (int i=0;i<3;i++) {
     out += analogRead(num);
     delayMicroseconds(30);
@@ -123,43 +87,135 @@ int readPoti(int num) {
 }
 
 
-//function to read mux
-int readMux(int channel, int no) {
+//Read a Value from the MUX
+int readMux(int channel, int no, bool analog) {
 
-  //loop through the four control pins
+  // Set the four MUX control pins
   for (int i = 0; i < 4; i ++) {
-    //turn on/off the appropriate control pins according to what channel we are trying to read
     digitalWrite(controlPin[i], muxChannel[channel][i]);
-    //digitalWrite(controlPin[i], bitRead(channel,3-i));
   }
 
-  int val;
-
-  //read the value of the pin
-  if ((no == 2 && ( (channel < 4) || (channel > 7 && channel < 12) )) || (no == 0 && !(channel < 12 && channel > 7))) {
-    pinMode(SIG_pin + no, INPUT_PULLUP);
+  if (analog) { // Read a analog value from a MUX
     delay(1);
-    val = digitalRead(SIG_pin + no);
+    return readPoti(SIG_pin + no);
+  } else { // Read a digital button value from a MUX
+    pinMode(SIG_pin + no, INPUT_PULLUP);
+    delayMicroseconds(50);
+    bool val = digitalRead(SIG_pin + no);
     pinMode(SIG_pin + no, INPUT);
+    return val;
   }
-  else val = readPoti(SIG_pin + no);
-  //return the value
-  return val;
 }
-int analogInsPrevTeensy[8]; //array to hold previously read analog values
-int tempAnalogInTeensy = 0;
-int tempAnalogInMapTeensy = 0;
-int analogThresholdTeensy = 4; //threshold
 
+
+// Reads a value from a MUX or directly from the Teensy
+int readValue(int channel, int no, bool analog) {
+  if (channel == 0) {
+    return readPoti(no);
+  } else {
+    return readMux(channel - 1, no, analog);
+  }
+}
+
+
+int analogThresholdTeensy = 4; //threshold to avoid jitter
+
+
+// All the pins, their function and their MIDI no [MIDIno, MUXno, PINno] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+// Analog values
+char anaConf[31][3] =
+{
+  // THE LOW MID HIGH MIXER VALUES
+  {0x01, 2, 12},
+  {0x02, 2, 13},
+  {0x03, 2, 14},
+  
+  {0x04, 2,  0},
+  {0x05, 2,  1},
+  {0x06, 2,  2},
+  
+  {0x07, 2,  8},
+  {0x08, 2,  9},
+  {0x09, 2, 10},
+  
+  {0x0A, 2,  4},
+  {0x0B, 2,  5},
+  {0x0C, 2,  6},
+  
+  // THE VOLUME VALUES
+  //{0x0D, 3,  8},
+  //{0x0E, 3, 11},
+  //{0x0F, 3,  9},
+  //{0x10, 3, 10},
+  
+  // HIGH PASS FILTERS
+  {0x11, 2, 15},
+  {0x12, 2,  3},
+  {0x13, 2, 11},
+  {0x14, 2,  7},
+  
+  // FX 1
+  {0x15, 1,  7},
+  {0x16, 1,  5},
+  {0x17, 1,  6},
+  {0x18, 1,  4},
+  // FX 2
+  {0x19, 1, 12},
+  {0x1A, 1, 13},
+  {0x1B, 1, 14},
+  {0x1C, 1, 15},
+};
+
+// Digital values (buttons)
+char btnConf[31][4] =
+{
+  // FX SELECT
+  // TODO
+
+  // FX1
+  {0x15, 1, 3,  1},
+  {0x16, 1, 2,  1},
+  {0x17, 1, 1,  1},
+  {0x18, 1, 0,  1},
+
+  // FX2
+  {0x19, 1, 8,  1},
+  {0x20, 1, 9,  1},
+  {0x21, 1, 10, 1},
+  {0x22, 1, 11, 1},
+
+
+  // DECK CONTROL LEFT
+  {0x20, 3, 0,  1}, //PLAY
+  {0x21, 3, 1,  1}, //CUE
+  {0x22, 3, 3,  1}, //SYNC
+  {0x23, 3, 4,  1}, //LOOP1
+  {0x24, 3, 5,  1}, //LOOP2
+  {0x25, 3, 6,  1}, //MOVE1
+  {0x26, 3, 7,  1}, //MOVE2
+
+   // DECK CONTROL RIGHT
+  {0x40, 0, 12, 1}, //PLAY
+  {0x41, 0, 12, 1}, //CUE
+  {0x42, 0, 12, 1}, //SYNC
+  {0x43, 0, 12, 1}, //LOOP1
+  {0x44, 0, 12, 1}, //LOOP2
+  {0x45, 0, 12, 1}, //MOVE1
+  {0x46, 0, 12, 1}, //MOVE2
+};
+
+// previous values
+int anaOld[31] = {};
+int digOld[31] = {};
 
 
 // SETUP (pin config) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 void setup() {
 
-  Serial.begin(31250);//open serail port @ midi speed
+  Serial.begin(31250);
 
   //PUSHBUTTON pin config
-  //we need enable each pushbutton pin as an INPUT as well as turn on the pullup resistor
   for (int i = 0; i < 38; i++) {
     if (toReadPushButton[i] == 1) {
       pinMode(i, INPUT_PULLUP); //pushbutton pullup
@@ -175,200 +231,98 @@ void setup() {
   usbMIDI.setHandleNoteOn(OnNoteOn) ;
   usbMIDI.setHandleNoteOff(OnNoteOff) ;
 
+  // Reset the encoder values
   enc1.write(64); enc2.write(64); enc3.write(64);
 
-  //SHIFT pin config
-  //we need enable the shift pin as an INPUT as well as turn on the pullup resistor
-  if (shiftPin != 0) {
-    pinMode(shiftPin, INPUT_PULLUP); //shift button
-  }
-
-  //ANALOG IN MUX pin config
+  // ANALOG IN MUX pin config
   //set analog in reading
   pinMode(SIG_pin, INPUT);
-  //set our control pins to output
-  pinMode(s0, OUTPUT);
-  pinMode(s1, OUTPUT);
-  pinMode(s2, OUTPUT);
-  pinMode(s3, OUTPUT);
-  //turn all control pins off (for now)
-  digitalWrite(s0, LOW);
-  digitalWrite(s1, LOW);
-  digitalWrite(s2, LOW);
-  digitalWrite(s3, LOW);
-
+  //set our control pins to low output
+  for (char i = 0; i < 4; i++) {
+     pinMode(controlPin[i], OUTPUT);
+     digitalWrite(controlPin[i], LOW);
+  }
 }
 
 
-
-// LOOPS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 void loop() {
 
-  // Loop through the Buttons
-  boolean tempDigitalRead;
-  for (int i = 0; i < 38; i++) { //loop through all 38 digital pins
-    int j = i + shiftChange; //add the shift change    TODO remove shift change
-    if (toReadPushButton[i] == 1) { //check if this a pin with a pushbutton hooked up to it
-      tempDigitalRead = digitalRead(i);
-      if (pushbuttonMode[j] == 1) { //normal mode
-        if (tempDigitalRead != buttonState[j]) { //something has changed
-          delay(2); //just a delay for noise to ensure push button was actually hit
-          if (digitalRead(i) == tempDigitalRead) { //check if pushbutton is still the same
-            if (tempDigitalRead == LOW) { //button pressed, turn note on
-              midiNoteOnOff(1, j); //call note on/off function
-            }
-            else { //button released
-              midiNoteOnOff(0, j);
-            }
-            buttonState[j] = tempDigitalRead; //update the state (on or off)
-          }
-        }
-      }
-      else { //all other modes (2,3,4)
-        if (digitalRead(i) == LOW) { //button was pressed
-          if ((millis() - timeHit[j]) > pbBounce) { //check bounce time
-            if (pushbuttonMode[j] == 2) { //mode 2 - only note on
-              midiNoteOnOff(1, j);
-            }
-            else if (pushbuttonMode[j] == 3) { //mode 3 - only note off
-              midiNoteOnOff(0, j);
-            }
-            else { //mode 4 - toggle
-              if (buttonState[j] == 1) {
-                midiNoteOnOff(0, j);
-                buttonState[j] = 0;
-              }
-              else {
-                midiNoteOnOff(1, j);
-                buttonState[j] = 1;
-              }
-            }
-            timeHit[j] = millis();
-          }
-        }
-      }
-    }
-  }
-
   // 3 Encoders: Jog wheels & Menu selector
-  
   int encVal[] = {enc1.read(), enc2.read(), enc3.read()};
-  
   for (int i = 0; i < 3; i++) {
     // Act on the encoder value
     if (encVal[i] != 64) {
-      //usbMIDI.sendNoteOn(i, encVal[i], 1);
       usbMIDI.sendControlChange(240+i, encVal[i], channelNumber);
     }
   }
   // Reset controllers to normal position
   enc1.write(64); enc2.write(64); enc3.write(64);
 
-  // Loop through all the multiplexers
-  for (int i = 0; i < 16; i++) {
-    if (!((i < 4) || (i > 7 && i < 12))) { //check if this a pin with a analog input hooked up to it
-      tempAnalogIn = readMux(i, 2); //ready valued using readMux function
-      if (abs(analogInsPrev[i] - tempAnalogIn) > analogThreshold) { //ensure value changed more than our threshold
-        if (i!=0&&i!=2||abs(analogInsPrev[i] - tempAnalogIn) > analogThreshold*3) {
-          tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
-          usbMIDI.sendControlChange(i, tempAnalogInMap, channelNumber);
-          analogInsPrev[i] = tempAnalogIn; //reset current value
-        }
-      }
-    } else {
-      bool tempDigitalRead = readMux(i, 2);
-      if (tempDigitalRead != analogInsPrev[i]) { //something has changed
-        delay(2); //just a delay for noise to ensure push button was actually hit
-        if (readMux(i, 2) == tempDigitalRead) { //check if pushbutton is still the same
-          if (tempDigitalRead == LOW) { //button pressed, turn note on
-            midiNoteOnOff(1, i + 76); //call note on/off function
-          } else { //button released
-            midiNoteOnOff(0, i + 76);
-          }
-          analogInsPrev[i] = tempDigitalRead; //update the state (on or off)
-        }
-      }
+
+
+  // Loop through analog values
+  for (char i = 0; i < 31; i++) {
+    int readVal = readMux(anaConf[i][2], anaConf[i][1], true);
+    if (abs(anaOld[i]-readVal) > analogThreshold) {
+      usbMIDI.sendControlChange(anaConf[i][0], map(readVal, 0, 1023, 0, 127), channelNumber);
+      anaOld[i] = readVal;
     }
   }
 
-  for (int i = 0; i < 16; i++) {
-    tempAnalogIn = readMux(i, 1); //ready valued using readMux function
-    if (abs(analogInsPrev[i + 16] - tempAnalogIn) > analogThreshold) { //ensure value changed more than our threshold
-      tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
-      usbMIDI.sendControlChange(i + 16, tempAnalogInMap, channelNumber);
-      analogInsPrev[i + 16] = tempAnalogIn; //reset current value
+
+  char nullChar[4] = {0,0,0,0};
+  
+  // Loop through digital values
+  for (char i = 0; i < 31; i++) {
+    if (btnConf[i] != nullChar) { // TODO
+    bool readVal = readMux(btnConf[i][2], btnConf[i][1], false);
+    if (readVal != digOld[i]) {
+      midiNoteOnOff(readVal, anaConf[i][0]);
+      digOld[i] = readVal;
+    }
     }
   }
 
-  for (int i = 0; i < 16; i++) {
-    tempAnalogIn = readMux(i, 0); //ready valued using readMux function
-    if (i < 12 && i > 7) {
-      if (abs(analogInsPrev[i + 32] - tempAnalogIn) > analogThreshold) { //ensure value changed more than our threshold
-        tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
-        usbMIDI.sendControlChange(i + 32, tempAnalogInMap, channelNumber);
-        analogInsPrev[i + 32] = tempAnalogIn; //reset current value
-      }
-    } else {
-      bool tempDigitalRead = readMux(i, 0);
-      if (tempDigitalRead != analogInsPrev[i + 32]) { //something has changed
-        delay(2); //just a delay for noise to ensure push button was actually hit
-        if (readMux(i, 0) == tempDigitalRead) { //check if pushbutton is still the same
-          if (tempDigitalRead == LOW) { //button pressed, turn note on
-            midiNoteOnOff(1, i + 92); //call note on/off function
-          } else { //button released
-            midiNoteOnOff(0, i + 92);
-          }
-          analogInsPrev[i + 32] = tempDigitalRead; //update the state (on or off)
-        }
-      }
-    }
-  }
+  delay(10);
 
-  //Loop through the Teensy analog channels
-  for (int i = 0; i < 4; i++) { //loop through the 8 analog teensy channels
-    tempAnalogInTeensy = readPoti(i + 39);
-    delay(2);
-    tempAnalogInTeensy = (tempAnalogInTeensy + readPoti(i + 39))/2;
-    if (abs(analogInsPrevTeensy[i] - tempAnalogInTeensy) > 1) {
-    if (i==0||i==2) {
-        if (loopOn[i/2]) {
-          tempAnalogInMapTeensy = map(tempAnalogInTeensy, 0, 1023, 0, 127);
-          usbMIDI.sendControlChange(i, tempAnalogInMapTeensy, channelNumber);
-        }
-    } else {
-        usbMIDI.sendControlChange(i, map(tempAnalogInTeensy, 0, 1023, 0, 127), channelNumber);
-        if (((i==1&&syncStatus[0]) || (i==3&&syncStatus[1])) ) {
-          tempAnalogInMapTeensy = map(tempAnalogInTeensy, 0, 1023, -8192, 8191);
-          usbMIDI.sendPitchBend(tempAnalogInMapTeensy, 3+(i-1)/2);
-        }
-      }
-      analogInsPrevTeensy[i] = tempAnalogInTeensy;
-    }
-  }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 // INIT all the analog values in case the controller has different values than the Program
 void initMUXvalues() {
 
+  int tempAnalogInTeensy = 0;
+  int tempAnalogInMapTeensy = 0;
+
   //ANALOG IN MUX loops
   for (int i = 0; i < 16; i++) {
     if (!((i < 4) || (i > 7 && i < 12))) { //check if this a pin with a analog input hooked up to it
-      tempAnalogIn = readMux(i, 2); //ready valued using readMux function
+      tempAnalogIn = readMux(i, 2, true); //ready valued using readMux function
       tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
       usbMIDI.sendControlChange(i, tempAnalogInMap, channelNumber);
     }
   }
 
   for (int i = 0; i < 16; i++) {
-    tempAnalogIn = readMux(i, 1); //ready valued using readMux function
+    tempAnalogIn = readMux(i, 1, true); //ready valued using readMux function
     tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
     usbMIDI.sendControlChange(i + 16, tempAnalogInMap, channelNumber);
   }
 
 
   for (int i = 0; i < 16; i++) {
-    tempAnalogIn = readMux(i, 0); //ready valued using readMux function
+    tempAnalogIn = readMux(i, 0, true); //ready valued using readMux function
     if (i < 12 && i > 7) {
       tempAnalogInMap = map(tempAnalogIn, 0, 1023, 0, 127); //remap value between 0 and 127
       usbMIDI.sendControlChange(i + 32, tempAnalogInMap, channelNumber);
@@ -390,8 +344,11 @@ void initMUXvalues() {
 }
 
 
-// LED Plink der Play Taste
+
+
+// LED Blink der Play Taste
 byte blinkLEDcounter = 0;
+
 void blinkLED() {
   usbMIDI.read();
   if (blinkLEDcounter == 0) {
@@ -416,8 +373,6 @@ void blinkLED() {
 
 // COMMUNICATION FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-//function to send note on/off
-//this helps to keep the code concise
 void midiNoteOnOff(boolean onoff, int q) {
   if (onoff == 1) { //note on
     usbMIDI.sendNoteOn(q, 127, channelNumber);
